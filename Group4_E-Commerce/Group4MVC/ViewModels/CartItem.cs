56162512092d4d92c43df41cac2761c6f5fcb7a3
Fadetz602 <145894019+Fadetz602@users.noreply.github.com>
2025-07-01 22:41:17 +0700

    Add to Cartfunction, edit product detail & index
    Add to Cartfunction, edit product detail & index

---
+++ Group4_E-Commerce/Group4MVC/ViewModels/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace GUI_Group4_ECommerce.ViewModels   
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string? Image { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public double Total => UnitPrice * Quantity;
+    }
+}
